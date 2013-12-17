1eb9f1545b93940497d7bfe332243a61b6f8989d
Freakkcd <ffr3akk@gmail.com>
2013-12-17 12:32:37 +0000

    OS and Browser optimization

--- js/drummachine.js
+++ js/drummachine.js
@@ -1,4 +1,37 @@
 $(document).ready(function () {
+    // OS Detection
+    var isWindows = (navigator.appVersion.indexOf("Win")!=-1);
+    var isMac = (navigator.appVersion.indexOf("Mac")!=-1);
+    var isUnix = (navigator.appVersion.indexOf("X11")!=-1);
+    var isLinux = (navigator.appVersion.indexOf("Linux")!=-1);
+    var isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1; //&& ua.indexOf("mobile");
+    
+    var OSName="Unknown OS";
+    if (isWindows) OSName="Windows";
+    if (isMac) OSName="MacOS";
+    if (isUnix) OSName="UNIX";
+    if (isLinux) OSName="Linux";
+    if (isAndroid) OSName="Android";
+    
+    console.log("OS: " + OSName);
+    
+    // Browser DeteCtion
+    var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0; // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+    var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0; // At least Safari 3+: "[object HTMLElementConstructor]"
+    var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+    var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+
+    var BrowserName="Unknown Browser";
+    if (isChrome) BrowserName="Chrome";
+    if (isFirefox) BrowserName="Firefox";
+    if (isSafari) BrowserName="Safari";
+    //if (isAndroidBrowser) BrowserName="AndroidBrowser";
+    if (isIE) BrowserName="IE";
+    if (isOpera) BrowserName="Opera";
+    
+    console.log("Browser: " + BrowserName);
+    if(isAndroid) alert(BrowserName+" on "+OSName);
     // get all files and fill the samples hasmap
     var activeKit="ac"; // the samples kit we're using
     var samplesHash = {};
@@ -100,34 +133,66 @@ $(document).ready(function () {
         play();
     });
     // TRANSPORT BAR CONTORL
-    var sequencerPlay =  function(measure,beat){
+    var sequencerPlay__Chrome =  function(measure,beat){
+            if(solo<0){
+                for( var trk = 0; trk<numTracks ; trk++){
+                    snd = $('#samples audio').get(samplesHash[$('#track_'+trk).attr('data-sound')]);
+                    if($('#track_'+trk+' #m'+measure+'-b'+beat).hasClass('active')){
+                        snd.currentTime = 0;
+                        snd.play();
+                    }
+                }
+            } else{
+                snd = $('#samples audio').get(samplesHash[$('#track_'+solo).attr('data-sound')]);
+                if($('#track_'+solo+' #m'+measure+'-b'+beat).hasClass('active')){
+                        snd.currentTime = 0;
+                        snd.play();
+                }
+            }
+    };
+    var sequencerPlay__OtherBrowsers =  function(measure,beat){    
+        // try this for browser that don't support currentTime efficiently (such as Firefox)
         if(solo<0){
-            for( var trk = 0; trk<numTracks ; trk++)
-            if($('#track_'+trk+' #m'+measure+'-b'+beat).hasClass('active')){
-                snd = samplesHash[$('#track_'+trk).attr('data-sound')];
-                $('#samples audio').get(snd).currentTime = 0;
-                $('#samples audio').get(snd).play();
+            for( var trk = 0; trk<numTracks ; trk++){
+                    snd = $('#samples audio').get(samplesHash[$('#track_'+trk).attr('data-sound')]);
+                    if($('#track_'+trk+' #m'+measure+'-b'+beat).hasClass('active')){
+                        snd.src = snd.src;
+                        snd.play();
+                    }
+                }
+            } else{
+                snd = $('#samples audio').get(samplesHash[$('#track_'+solo).attr('data-sound')]);
+                if($('#track_'+solo+' #m'+measure+'-b'+beat).hasClass('active')){
+                        snd.src = snd.src;
+                        snd.play();
+                }
             }
-        } else{
-            if($('#track_'+solo+' #m'+measure+'-b'+beat).hasClass('active')){
-                snd = samplesHash[$('#track_'+solo).attr('data-sound')];
-                $('#samples audio').get(snd).currentTime = 0;
-                $('#samples audio').get(snd).play();
-            }
-        }
      };
            
     var play = function(){
         if(!playing){
             playing = true;
-            sequencerPlaying = setInterval(function() {
-                sequencerPlay(currMeasure,currBeat);
-                updateBeatDisplay();
-                if(++currBeat >= numBeats){
-                    currBeat=0;
-                    if(++currMeasure >= numMeasures) currMeasure=0;
-                }
-             }, 60*1000/bpm);
+            
+            if( isChrome || isSafari ) {
+                sequencerPlaying = setInterval(function() {
+                    sequencerPlay__Chrome(currMeasure,currBeat);
+                    updateBeatDisplay();
+                    if(++currBeat >= numBeats){
+                        currBeat=0;
+                        if(++currMeasure >= numMeasures) currMeasure=0;
+                    }
+                }, 60*1000/bpm);
+            }
+            else{ 
+                sequencerPlaying = setInterval(function() {
+                    sequencerPlay__OtherBrowsers(currMeasure,currBeat);
+                    updateBeatDisplay();
+                    if(++currBeat >= numBeats){
+                        currBeat=0;
+                        if(++currMeasure >= numMeasures) currMeasure=0;
+                    }
+                }, 60*1000/bpm);
+            }
         }
     };       
 
